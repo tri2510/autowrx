70a0e51c7deae171493b61681c7c4f5caed61387
NhanLuong <nhan.luongnguyen@vn.bosch.com>
2024-05-21 17:25:20 +0700

    Add Block component in molecules

---
+++ src/components/molecules/Block.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Block = ({}) => {
+  // const [x, setX] = useState("");
+  return (
+    <div className="w-full min-h-[48px] px-8 py-4 bg-slate-500 text-white flex justify-center align-center">
+      Block
+    </div>
+  );
+};
+
+export { Block };
