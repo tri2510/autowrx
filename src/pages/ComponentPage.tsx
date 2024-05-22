e472e298f2ff573db342556b46d0fe42ca00e58d
Tam Thai Hoang Minh <115630638+TamThaiHoangMinh@users.noreply.github.com>
2024-05-22 18:10:27 +0700

    Add DaDropdown and DaPaging based on Shadcn Components

--- src/pages/ComponentPage.tsx
+++ src/pages/ComponentPage.tsx
@@ -1,14 +1,29 @@
-import { Block } from "@/components/molecules/Block";
 import { DaButton } from "@/components/atoms/DaButton";
 import { DaText } from "@/components/atoms/DaText";
 import { DaInput } from "@/components/atoms/DaInput";
 import { DaTag } from "@/components/atoms/DaTag";
 import { DaImage } from "@/components/atoms/DaImage";
 import { DaAvatar } from "@/components/atoms/DaAvatar";
+import {
+  DaPaging,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/atoms/DaPaging";
+import {
+  DaDropdown,
+  DropdownContent,
+  DropdownTrigger,
+  DropdownValue,
+  DropdownItem,
+} from "@/components/atoms/DaDropdown";
 
 const PageHome = () => {
   return (
-    <div className="grid grid-cols-12 gap-4 p-4 bg-slate-100">
+    <div className="grid grid-cols-12 gap-4 p-4 bg-da-white">
       <div className="col-span-full space-x-4 p-4 border rounded-lg">
         <DaButton variant={"default"}>Default Button</DaButton>
         <DaButton variant={"outline"}>Outline Button</DaButton>
@@ -38,14 +53,47 @@ const PageHome = () => {
         <DaImage src="imgs/3.jpg" width={400} ratio={16 / 9} />
       </div>
       <div className="flex col-span-full space-x-4 p-4 border rounded-lg">
-        <DaImage src="imgs/1.jpg" width={400} ratio={16 / 9} />
-        <DaImage src="imgs/2.jpg" width={400} ratio={16 / 9} />
-        <DaImage src="imgs/3.jpg" width={400} ratio={16 / 9} />
-      </div>
-      <div className="flex col-span-full space-x-4 p-4 border rounded-lg">
         <DaAvatar src="imgs/1.jpg" />
         <DaAvatar src="imgs/2.jpg" className="w-16 h-16" />
         <DaAvatar src="imgs/3.jpg" className="w-24 h-24" />
+      </div>
+      <div className="flex col-span-full space-x-4 p-4 border rounded-lg">
+        <DaPaging>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                2
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </DaPaging>
+      </div>
+      <div className="flex col-span-full space-x-4 p-4 border rounded-lg">
+        <DaDropdown>
+          <DropdownTrigger className="w-[180px]">
+            <DropdownValue placeholder="Theme" />
+          </DropdownTrigger>
+          <DropdownContent>
+            <DropdownItem value="light">Light</DropdownItem>
+            <DropdownItem value="dark">Dark</DropdownItem>
+            <DropdownItem value="system">System</DropdownItem>
+          </DropdownContent>
+        </DaDropdown>
       </div>
     </div>
   );
