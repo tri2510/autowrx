807e1eec4f2db92e8e5de62b69fa7e7e8592a3be
NhanLuong <nhan.luongnguyen@vn.bosch.com>
2024-05-22 11:04:49 +0700

    Change Block class style

--- src/components/molecules/Block.tsx
+++ src/components/molecules/Block.tsx
@@ -1,33 +1,23 @@
-import { useState, FC, useEffect } from "react";
+import { FC } from "react";
 
 interface BlockProps {
   title: string;
-  variant?: "solid" | "outline";
+  variant?: "default" | "outline";
   height?: string;
   className?: string;
 }
 
 const Block: FC<BlockProps> = ({
   title,
-  variant = "solid",
+  variant = "default",
   height = "200px",
   className = "",
 }) => {
-  const [variantStyle, setVariantStyle] = useState<string>("");
-  useEffect(() => {
-    let style = "px-4 py-2 flex items-center justify-center text-bold";
-    switch (variant) {
-      case "outline":
-        style += " text-primary-700 border border-primary-500";
-        break;
-      default:
-        style += " text-white bg-primary-500";
-    }
-    setVariantStyle(style);
-  }, [variant]);
-
   return (
-    <div className={`${variantStyle} ${className}`} style={{ height: height }}>
+    <div
+      className={`da-block-bare da-block-${variant} ${className}`}
+      style={{ height: height }}
+    >
       {title}
     </div>
   );
