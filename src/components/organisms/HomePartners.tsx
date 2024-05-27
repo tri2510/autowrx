741a931cf704d48c13bcd32d7b216d0b79ed1bac
NhanLuong <nhan.luongnguyen@vn.bosch.com>
2024-05-27 11:04:40 +0700

    Change instance.js to instance.ts, remove unused code

--- src/components/organisms/HomePartners.tsx
+++ src/components/organisms/HomePartners.tsx
@@ -2,46 +2,6 @@ import { FC } from "react";
 import { DaText } from "../atoms/DaText";
 import { DaImage } from "../atoms/DaImage";
 import { useParnerList } from "@/hooks/useInstanceCfg";
-import { Link } from "react-router-dom";
-
-const partnersData = [
-  {
-    category: "Industry Partners",
-    partners: [
-      {
-        imageUrl: "https://digitalauto.netlify.app/assets/Bosch-2cbabb81.png",
-        alt: "Industry Partner 1",
-      },
-      {
-        imageUrl:
-          "https://digitalauto.netlify.app/assets/Dassault-db92600b.png",
-        alt: "Industry Partner 2",
-      },
-    ],
-  },
-  {
-    category: "Standards & Open Source",
-    partners: [
-      {
-        imageUrl: "https://digitalauto.netlify.app/assets/COVESA-b3f64c5b.png",
-        alt: "Standards Partner 1",
-      },
-      {
-        imageUrl: "https://digitalauto.netlify.app/assets/Eclipse-ff73acad.png",
-        alt: "Standards Partner 2",
-      },
-    ],
-  },
-  {
-    category: "Academic Partners",
-    partners: [
-      {
-        imageUrl: "https://digitalauto.netlify.app/assets/FSTI-55cf60eb.png",
-        alt: "Academic Partner 1",
-      },
-    ],
-  },
-];
 
 const HomePartners: FC = () => {
   const partners = useParnerList();
