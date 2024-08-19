70ffdcdf5c2e225c909380c62076d9dcd0af03f2
Luong Nguyen Nhan (MS/ETA4) <nhan.luongnguyen@vn.bosch.com>
2024-08-19 16:53:36 +0700

    support command write_signals_value

--- src/components/molecules/dashboard/DaApisWatch.tsx
+++ src/components/molecules/dashboard/DaApisWatch.tsx
@@ -1,8 +1,48 @@
-import { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import useRuntimeStore from '@/stores/runtimeStore'
 import { shallow } from 'zustand/shallow'
 
-const DaApisWatch: FC = ({}) => {
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+    className?: string
+    onEnter?: (value:string) => void
+}
+
+const ApiInput = React.forwardRef<HTMLInputElement, InputProps>(
+    (
+      {
+        className,
+        onEnter,
+        ...props
+      },
+      ref,
+    )  => {
+    const [value, setValue] = useState("")
+    return <input className={
+                `w-16 flex px-2 py-1 h-6 ml-2 rounded
+                text-da-gray-dark text-right font-semibold
+                focus-visible:ring-0 focus-visible:outline-none
+                disabled:cursor-not-allowed`}
+            ref={ref}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={(e) => {
+                if (e.key === "Enter")
+                    if(onEnter) {
+                        onEnter(value)
+                        setValue("")
+                    }
+                }}
+            {...props}/>
+})
+
+export interface DaApisWatchProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+    requestWriteSignalValue?: (obj: any) => void
+}
+
+
+const DaApisWatch: FC<DaApisWatchProps> = ({requestWriteSignalValue}) => {
 
     const [apisValue] = useRuntimeStore(
         (state) => [
@@ -12,10 +52,24 @@ const DaApisWatch: FC = ({}) => {
     )
 
     return <div className="w-full mt-2">
-        { apisValue && Object.keys(apisValue).map((key) => <div key={key} className={`px-1 flex items-center border-da-gray-medium 
+        { apisValue && Object.keys(apisValue).map((key: string) => <div key={key} className={`px-1 flex items-center border-da-gray-medium 
             text-da-white da-label-tiny mb-1`}>
             <div className='grow px-1'>{key}</div>
-            <div className='w-20 px-1 py-0.5 text-right border border-da-gray-medium rounded'>{String(apisValue[key]) || 'null'}</div>
+            <div className='w-16 px-1 py-0.5 text-right border border-da-gray-medium rounded'>{String(apisValue[key]) || 'null'}</div>
+            <ApiInput onEnter={(value:string) => {
+                // console.log("onEntered", value)
+                let sendValue = null
+                if(["true", "false"].includes(value.trim().toLowerCase())) {
+                    sendValue = (value.trim().toLowerCase()=='true')
+                } else {
+                    sendValue = Number(value)
+                }
+                if(value.length>0 && requestWriteSignalValue) {
+                    let payload = {} as any
+                    payload[`${key}`] = sendValue
+                    requestWriteSignalValue(payload)
+                }
+            }}/>
         </div>)
         }
     </div>
