28f4f102b7ce91d7ea81c6fda89dbbf2c9ad1459
tuanh <tuan.hoangdinhanh@vn.bosch.com>
2024-09-17 17:32:10 +0700

    revert default config for autowrx

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,7 +1,5 @@
 const config: any = {
-  serverBaseUrl:
-    import.meta.env.VITE_SERVER_BASE_URL ||
-    'https://backend-core-dev.digital.auto',
+  serverBaseUrl: 'https://backend-core-dev.digital.auto',
   serverVersion: import.meta.env.VITE_SERVER_VERSION || 'v2',
   logBaseUrl: import.meta.env.PROD
     ? 'https://logs.digital.auto'
@@ -12,27 +10,18 @@ const config: any = {
   studioBeUrl: 'https://bewebstudio.digitalauto.tech',
   widgetMarketPlaceUrl: 'https://marketplace.digital.auto/packagetype/widget',
   widgetMarketPlaceBe: 'https://store-be.digitalauto.tech',
-  uploadFileUrl: 'https://upload.digitalauto.asia',
+  uploadFileUrl: 'https://upload.digitalauto.tech',
   instanceLogo:
-    'https://static.wixstatic.com/media/604381_7624c1d2f72b45c18183be743e983e45~mv2.png/v1/fill/w_77,h_77,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo-Icon.png',
-  instance: 'digitalauto',
+    'https://covesa.global/wp-content/uploads/2024/03/covesa_logo.png',
   sso: 'bosch',
-  defaultModelId: '667a9194694a35002f961ba8',
+  instance: 'digitalauto',
+  defaultModelId: '665826e3194aff003dd2f67b',
   genAI: {
     wizardCover: '/imgs/default_prototype_cover.jpg',
-    wizardPreferRuntime: 'RunTime-VSS4-02-20',
     hideMarketplace: false,
-    defaultEndpointUrl: 'https://backend-core-etas.digital.auto/v2/genai',
+    defaultEndpointUrl: 'https://backend-core-dev.digital.auto/v2/genai',
+    marketplaceUrl: 'https://store-be.digitalauto.tech/marketplace/genai',
     sdvApp: [
-      {
-        id: 'etas-sdv-genai',
-        type: 'GenAI_Python',
-        name: 'ETAS SDV GenAI',
-        description: 'ETAS GenAI for Python code generation',
-        apiKey: 'Empty',
-        endpointUrl: 'https://backend-core-etas.digital.auto/v2/genai/etas',
-        customPayload: (prompt: string) => ({ prompt }),
-      },
       {
         id: 'mock-genai',
         type: 'GenAI_Python',
@@ -44,25 +33,17 @@ const config: any = {
         isMock: true,
       },
     ],
-    dashboard: [
-      {
-        id: 'etas-dashboard-genai',
-        type: 'GenAI_Python',
-        name: 'ETAS Dashboard GenAI',
-        description: 'ETAS GenAI for Python code generation',
-        apiKey: 'Empty',
-        endpointUrl: 'https://backend-core-etas.digital.auto/v2/genai/etas',
-        customPayload: (prompt: string) => ({ prompt }),
-      },
-    ],
+    dashboard: [],
     widget: [],
   },
   github: {
-    clientId: import.meta.env.VITE_GITHUB_CLIENT_ID || '',
+    clientId: 'Ov23livVYo2MXyqoIHox',
   },
-  features: [],
-  strictAuth: import.meta.env.VITE_STRICT_AUTH === 'true',
-  disableEmailLogin: import.meta.env.VITE_DISABLE_EMAIL_LOGIN === 'true',
+  disableEmailLogin: false,
+  runtime: {
+    url: 'https://kit.digitalauto.tech',
+  },
+  strictAuth: false,
 }
 
 export default config
