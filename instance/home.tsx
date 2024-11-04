e01cd8dbacf362fc19d0890a950a361358bd4801
Tuan Hoang Dinh Anh <tuan.hoangdinhanh@vn.bosch.com>
2024-11-04 05:36:45 +0000

    Update urls of news in home.tsx

--- instance/home.tsx
+++ instance/home.tsx
@@ -1,3 +1,4 @@
+import { title } from 'process'
 import { BsStars } from 'react-icons/bs'
 import { FaCar } from 'react-icons/fa'
 import { TbArrowRight, TbCode, TbPackageImport } from 'react-icons/tb'
@@ -64,13 +65,60 @@ const home = [
     ],
   },
   {
+    type: 'news',
+    title: 'Top News',
+    items: [
+      {
+        title: 'digital.auto Meetup',
+        type: 'Event',
+        date: '26 November 2024',
+        description:
+          'Join Prof. Dirk Slama to discuss the roadmap and priorities of our digital.auto community in 2025. Meet Fabio Violante (CEO Arduino) to discuss our plans for an open E/E Starter Kit for SDVs',
+        imageURL:
+          'https://bewebstudio.digitalauto.tech/data/projects/VOMpcykPhfIC/digital.auto%20Meetup.png',
+        redirectURL:
+          'https://www.linkedin.com/posts/digitalauto_join-prof-dirk-slama-to-discuss-the-roadmap-activity-7255901526752219137-RFLH?utm_source=share&utm_medium=member_desktop',
+      },
+      {
+        title: 'Zonal E/E architecture | digital.auto x MiX',
+        date: '25 October 2024',
+        type: 'News',
+        description:
+          'The Bosch MiX project has developed a demonstrator platform for real-time applications in a zonal E/E architecture utilizing NXP hashtag#S32G3 and Yocto Linux.',
+        imageURL:
+          'https://bewebstudio.digitalauto.tech/data/projects/VOMpcykPhfIC/Central%20Compute%20with%20Zones.png',
+        redirectURL: 'https://www.youtube.com/watch?v=M2o3h4anJ_s',
+      },
+      {
+        title: 'digital.auto at ETAS Connections 2024',
+        date: '9 October 2024',
+        type: 'Event',
+        description:
+          `Check out the digital.auto demo stand and don't miss the panel discussion on continous homologation`,
+        imageURL:
+          'https://media.licdn.com/dms/image/v2/D5622AQGDWg4yDt6r6A/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1728543615217?e=1732752000&v=beta&t=X5_hY7cHDgph813Mn2O4I7sctrcxv_wWdXQ7IVYYThU',
+        redirectURL:
+          'https://www.linkedin.com/posts/digitalauto_etas-connections-2024-panel-session-activity-7240339277119705088-bEhz?utm_source=share&utm_medium=member_desktop',
+      },
+      {
+        title: 'GenAI Awards',
+        type: 'Event',
+        date: '1 January 2025',
+        description:
+          'Join us to harness the power of GenAI for the software-defined vehicle (SDV). Win the digital.auto GenAI award by participating in our quarterly competitions. We are looking for novel ways to apply multimodel GenAI to create visually rich',
+        imageURL:
+          'https://bewebstudio.digitalauto.tech/data/projects/8go3BVLvQX3B/GenAI_Cover.png',
+        redirectURL: 'https://www.digital.auto/genai-award',
+      },
+    ],
+  },
+  {
     type: 'recent',
     title: 'Recent Prototypes',
   },
   {
     type: 'popular',
     title: 'Popular Prototypes',
-    requiredLogin: true,
   },
   {
     type: 'partner-list',
