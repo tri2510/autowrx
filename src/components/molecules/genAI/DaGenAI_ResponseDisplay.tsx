28b808added6b03150bbff2c61dcdf8910310de0
tuanh <tuan.hoangdinhanh@vn.bosch.com>
2024-11-14 11:50:31 +0700

    dynamic load highlight.js library

--- src/components/molecules/genAI/DaGenAI_ResponseDisplay.tsx
+++ src/components/molecules/genAI/DaGenAI_ResponseDisplay.tsx
@@ -1,6 +1,10 @@
-import SyntaxHighlighter from 'react-syntax-highlighter'
 import { githubGist } from 'react-syntax-highlighter/dist/esm/styles/hljs'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, lazy, Suspense } from 'react'
+import { retry } from '@/lib/retry'
+
+const SyntaxHighlighter = lazy(() =>
+  retry(() => import('react-syntax-highlighter/dist/esm/prism-light')),
+) as any
 
 export const customStyle = {
   ...githubGist,
@@ -24,13 +28,15 @@ const DaGenAI_ResponseDisplay = ({ language = 'text', code = '' }) => {
 
   return (
     <div className="scroll-gray flex h-full w-full overflow-y-auto rounded-md border border-gray-200 bg-gray-50 shadow">
-      <SyntaxHighlighter
-        language={language}
-        style={customStyle}
-        className="scroll-gray flex w-full text-xs"
-      >
-        {genCode}
-      </SyntaxHighlighter>
+      <Suspense>
+        <SyntaxHighlighter
+          language={language}
+          style={customStyle}
+          className="scroll-gray flex w-full text-xs"
+        >
+          {genCode}
+        </SyntaxHighlighter>
+      </Suspense>
     </div>
   )
 }
