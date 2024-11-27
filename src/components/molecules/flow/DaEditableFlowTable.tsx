18d86b7aebacc63926e94fdc3e7352ce3623af8f
Tam Thai Hoang Minh <tam.thaihoangminh@vn.bosch.com>
2024-11-27 17:32:37 +0700

    Add saving flow saving mechanism

--- src/components/molecules/flow/DaEditableFlowTable.tsx
+++ src/components/molecules/flow/DaEditableFlowTable.tsx
@@ -242,171 +242,207 @@ const DaPrototypeFlowEditor = ({
     setData(newData)
   }
 
+  const handleSave = () => {
+    // Filter out empty steps (steps with no flows)
+    const cleanedData = data.filter((step) => step.flows.length > 0)
+
+    // Convert to JSON string
+    const jsonString = JSON.stringify(cleanedData)
+
+    // Call the onSave prop with the JSON string
+    onSave(jsonString)
+  }
+
   return (
     <div className="flex flex-col w-full h-full">
-      <table className=" table-fixed w-full">
-        <colgroup>
-          <col className="w-[11.11%]" />
-          <col className="w-[11.11%]" />
-          <col className="w-[11.11%]" />
-          <col className="w-[11.11%]" />
-          <col className="w-[11.11%]" />
-          <col className="w-[11.11%]" />
-          <col className="w-[11.11%]" />
-          <col className="w-[11.11%]" />
-          <col className="w-[11.11%]" />
-        </colgroup>
-        <thead>
-          <tr className="text-sm text-white uppercase">
-            <th
-              colSpan={3}
-              className="bg-da-primary-100 text-da-primary-500 border border-r-transparent font-semibold p-2 "
-            >
-              Off-board
-            </th>
-            <th className="border border-r-transparent"></th>
-            <th
-              colSpan={5}
-              className="bg-da-primary-500 border font-semibold p-2"
-            >
-              On-board
-            </th>
-          </tr>
-          <tr className="text-xs text-da-gray-dark uppercase">
-            <th className="border p-2">Smart Phone</th>
+      <div className="flex items-center justify-between border-b pb-2 mb-4 ">
+        <h2 className="text-sm font-semibold text-da-primary-500">
+          End-to-End Flow: Smart Trunk
+        </h2>
+        <div className="flex items-center space-x-2">
+          <DaButton
+            onClick={onCancel}
+            className="w-[60px]"
+            variant="outline-nocolor"
+            size="sm"
+          >
+            Cancel
+          </DaButton>
+          <DaButton
+            onClick={handleSave}
+            className="w-[60px]"
+            variant="solid"
+            size="sm"
+          >
+            Save
+          </DaButton>
+        </div>
+      </div>
+      <div className="flex flex-col w-full h-full overflow-y-auto">
+        <table className=" table-fixed w-full overflow">
+          <colgroup>
+            <col className="w-[11.11%]" />
+            <col className="w-[11.11%]" />
+            <col className="w-[11.11%]" />
+            <col className="w-[11.11%]" />
+            <col className="w-[11.11%]" />
+            <col className="w-[11.11%]" />
+            <col className="w-[11.11%]" />
+            <col className="w-[11.11%]" />
+            <col className="w-[11.11%]" />
+          </colgroup>
+          <thead>
+            <tr className="text-sm text-white uppercase">
+              <th
+                colSpan={3}
+                className="bg-da-primary-100 text-da-primary-500 border border-r-transparent font-semibold p-2 "
+              >
+                Off-board
+              </th>
+              <th className="border border-r-transparent"></th>
+              <th
+                colSpan={5}
+                className="bg-da-primary-500 border font-semibold p-2"
+              >
+                On-board
+              </th>
+            </tr>
+            <tr className="text-xs text-da-gray-dark uppercase">
+              <th className="border p-2">Smart Phone</th>
 
-            <th className="border p-2">
-              <DaTooltip content="Phone to Cloud" className="normal-case">
-                <div className="cursor-pointer">p2c</div>
-              </DaTooltip>
-            </th>
+              <th className="border p-2">
+                <DaTooltip content="Phone to Cloud" className="normal-case">
+                  <div className="cursor-pointer">p2c</div>
+                </DaTooltip>
+              </th>
 
-            <th className="border p-2">Cloud</th>
-            <th className="border p-2">
-              <DaTooltip content="Vehicle to Cloud" className="normal-case">
-                <div className="cursor-pointer">v2c</div>
-              </DaTooltip>
-            </th>
-            <th className="border p-2">SDV Runtime</th>
-            <th className="border p-2">
-              <DaTooltip content="System to System" className="normal-case">
-                <div className="cursor-pointer">s2s</div>
-              </DaTooltip>
-            </th>
-            <th className="border p-2">Embedded</th>
-            <th className="border p-2">
-              <DaTooltip content="System to ECU" className="normal-case">
-                <div className="cursor-pointer">s2e</div>
-              </DaTooltip>
-            </th>
-            <th className="border p-2 truncate">Sensors/Actuators</th>
-          </tr>
-        </thead>
+              <th className="border p-2">Cloud</th>
+              <th className="border p-2">
+                <DaTooltip content="Vehicle to Cloud" className="normal-case">
+                  <div className="cursor-pointer">v2c</div>
+                </DaTooltip>
+              </th>
+              <th className="border p-2">SDV Runtime</th>
+              <th className="border p-2">
+                <DaTooltip content="System to System" className="normal-case">
+                  <div className="cursor-pointer">s2s</div>
+                </DaTooltip>
+              </th>
+              <th className="border p-2">Embedded</th>
+              <th className="border p-2">
+                <DaTooltip content="System to ECU" className="normal-case">
+                  <div className="cursor-pointer">s2e</div>
+                </DaTooltip>
+              </th>
+              <th className="border p-2 truncate">Sensors/Actuators</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {data.map((step, stepIndex) => (
-            <React.Fragment key={stepIndex}>
-              <tr>
-                <td colSpan={9} className="border p-2">
-                  <ContextMenu>
-                    <ContextMenuTrigger>
-                      <DaInput
-                        type="text"
-                        value={step.title}
-                        onChange={(e) => {
-                          const newData = [...data]
-                          newData[stepIndex].title = e.target.value
-                          setData(newData)
-                        }}
-                        className="w-full rounded !text-xs"
-                        inputClassName="text-sm font-medium text-da-primary-500 bg-da-primary-100"
-                        wrapperClassName="bg-da-primary-100 border-da-primary-300 hover:border-da-primary-500"
-                      />
-                    </ContextMenuTrigger>
-                    <ContextMenuContent className="bg-white z-100">
-                      <ContextMenuItem
-                        className="cursor-pointer hover:text-red-500"
-                        onClick={() => deleteStep(stepIndex)}
-                      >
-                        <TbTrash className="size-4 mr-1" />
-                        Delete Step
-                      </ContextMenuItem>
-                    </ContextMenuContent>
-                  </ContextMenu>
-                </td>
-              </tr>
-              {step.flows.map((flow, flowIndex) => (
-                <tr key={flowIndex}>
-                  {FLOW_CELLS.map((cell) => (
+          <tbody>
+            {data.map((step, stepIndex) => (
+              <React.Fragment key={stepIndex}>
+                <tr>
+                  <td colSpan={9} className="border p-2">
                     <ContextMenu>
-                      <td
-                        key={cell.key}
-                        className={`border p-2 ${cell.key === 'v2c' ? '' : ''}`}
-                      >
-                        <ContextMenuTrigger>
-                          {cell.isSignalFlow ? (
-                            <SignalFlowEditor
-                              flow={getNestedValue(flow, cell.path)}
-                              onChange={(newFlow) =>
-                                updateFlow(
-                                  stepIndex,
-                                  flowIndex,
-                                  cell.path,
-                                  newFlow,
-                                )
-                              }
-                            />
-                          ) : (
-                            <TextCell
-                              value={getNestedValue(flow, cell.path)}
-                              onChange={(value) =>
-                                updateFlow(
-                                  stepIndex,
-                                  flowIndex,
-                                  cell.path,
-                                  value,
-                                )
-                              }
-                            />
-                          )}
-                          <ContextMenuContent className="bg-white z-100">
-                            <ContextMenuItem
-                              className="cursor-pointer hover:text-red-500"
-                              onClick={() => deleteFlow(stepIndex, flowIndex)}
-                            >
-                              <TbTrash className="size-4 mr-1" />
-                              Delete Flow
-                            </ContextMenuItem>
-                          </ContextMenuContent>
-                        </ContextMenuTrigger>
-                      </td>
+                      <ContextMenuTrigger>
+                        <DaInput
+                          type="text"
+                          value={step.title}
+                          onChange={(e) => {
+                            const newData = [...data]
+                            newData[stepIndex].title = e.target.value
+                            setData(newData)
+                          }}
+                          className="w-full rounded !text-xs"
+                          inputClassName="text-sm font-medium text-da-primary-500 bg-da-primary-100"
+                          wrapperClassName="bg-da-primary-100 border-da-primary-300 hover:border-da-primary-500"
+                        />
+                      </ContextMenuTrigger>
+                      <ContextMenuContent className="bg-white z-100">
+                        <ContextMenuItem
+                          className="cursor-pointer hover:text-red-500"
+                          onClick={() => deleteStep(stepIndex)}
+                        >
+                          <TbTrash className="size-4 mr-1" />
+                          Delete Step
+                        </ContextMenuItem>
+                      </ContextMenuContent>
                     </ContextMenu>
-                  ))}
+                  </td>
                 </tr>
-              ))}
-              <tr>
-                <td colSpan={9} className="border-x p-2">
-                  <DaButton
-                    onClick={() => addFlowToStep(stepIndex)}
-                    size="sm"
-                    variant="dash"
-                    className="w-full"
-                  >
-                    <TbPlus className="size-4 mr-2" /> Add Flow
-                  </DaButton>
-                </td>
-              </tr>
-            </React.Fragment>
-          ))}
-          <tr>
-            <td colSpan={9} className="px-2">
-              <DaButton onClick={addStep} className="w-full" size="sm">
-                <TbPlus className="size-4 mr-2" /> Add Step
-              </DaButton>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                {step.flows.map((flow, flowIndex) => (
+                  <tr key={flowIndex}>
+                    {FLOW_CELLS.map((cell) => (
+                      <ContextMenu>
+                        <td
+                          key={cell.key}
+                          className={`border p-2 ${cell.key === 'v2c' ? '' : ''}`}
+                        >
+                          <ContextMenuTrigger>
+                            {cell.isSignalFlow ? (
+                              <SignalFlowEditor
+                                flow={getNestedValue(flow, cell.path)}
+                                onChange={(newFlow) =>
+                                  updateFlow(
+                                    stepIndex,
+                                    flowIndex,
+                                    cell.path,
+                                    newFlow,
+                                  )
+                                }
+                              />
+                            ) : (
+                              <TextCell
+                                value={getNestedValue(flow, cell.path)}
+                                onChange={(value) =>
+                                  updateFlow(
+                                    stepIndex,
+                                    flowIndex,
+                                    cell.path,
+                                    value,
+                                  )
+                                }
+                              />
+                            )}
+                            <ContextMenuContent className="bg-white z-100">
+                              <ContextMenuItem
+                                className="cursor-pointer hover:text-red-500"
+                                onClick={() => deleteFlow(stepIndex, flowIndex)}
+                              >
+                                <TbTrash className="size-4 mr-1" />
+                                Delete Flow
+                              </ContextMenuItem>
+                            </ContextMenuContent>
+                          </ContextMenuTrigger>
+                        </td>
+                      </ContextMenu>
+                    ))}
+                  </tr>
+                ))}
+                <tr>
+                  <td colSpan={9} className="border-x p-2">
+                    <DaButton
+                      onClick={() => addFlowToStep(stepIndex)}
+                      size="sm"
+                      variant="dash"
+                      className="w-full"
+                    >
+                      <TbPlus className="size-4 mr-2" /> Add Flow
+                    </DaButton>
+                  </td>
+                </tr>
+              </React.Fragment>
+            ))}
+            <tr>
+              <td colSpan={9} className="px-2">
+                <DaButton onClick={addStep} className="w-full" size="sm">
+                  <TbPlus className="size-4 mr-2" /> Add Step
+                </DaButton>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
--- src/components/organisms/PrototypeTabFlow.tsx
+++ src/components/organisms/PrototypeTabFlow.tsx
@@ -3,7 +3,10 @@ import { TbArrowLeft, TbArrowRight, TbArrowsHorizontal } from 'react-icons/tb'
 import useCurrentPrototype from '@/hooks/useCurrentPrototype'
 import DaTooltip from '../atoms/DaTooltip'
 import { FlowStep, Direction, SignalFlow } from '@/types/flow.type'
-import { flowData } from '../molecules/flow/flowMockData'
+import {
+  objectFlowData,
+  stringifiedFlowData,
+} from '../molecules/flow/flowMockData'
 import DaPrototypeFlowEditor from '../molecules/flow/DaEditableFlowTable'
 import { DaButton } from '../atoms/DaButton'
 
@@ -32,10 +35,14 @@ const SignalFlowCell: React.FC<{ flow: SignalFlow | null }> = ({ flow }) => {
 const PrototypeTabFlow = () => {
   const { data: prototype } = useCurrentPrototype()
   const [isEditing, setIsEditing] = React.useState(false)
+  const [flowData, setFlowData] = React.useState<FlowStep[]>(
+    JSON.parse(stringifiedFlowData),
+  )
 
-  const handleSave = async (jsonData: string) => {
+  const handleSave = async (stringJsonData: string) => {
     try {
-      console.log('Saving flow data:', jsonData)
+      console.log('Saving flow data:', stringJsonData)
+      setFlowData(JSON.parse(stringJsonData))
       setIsEditing(false)
     } catch (error) {
       console.error('Error saving flow data:', error)
@@ -45,20 +52,6 @@ const PrototypeTabFlow = () => {
   return (
     <div className="flex w-full h-full p-2 gap-2 bg-slate-100 text-xs">
       <div className="flex w-full h-full flex-col bg-white rounded-md p-4">
-        <div className="flex items-center justify-between border-b pb-2 mb-4 ">
-          <h2 className="text-sm font-semibold text-da-primary-500">
-            End-to-End Flow: Smart Trunk
-          </h2>
-          <DaButton
-            onClick={() => setIsEditing(!isEditing)}
-            className="w-[60px] mb-4"
-            variant="solid"
-            size="sm"
-          >
-            {isEditing ? 'Cancel' : 'Edit'}
-          </DaButton>
-        </div>
-
         <div className="w-full overflow-x-auto">
           {isEditing ? (
             <DaPrototypeFlowEditor
@@ -67,113 +60,135 @@ const PrototypeTabFlow = () => {
               onCancel={() => setIsEditing(false)}
             />
           ) : (
-            <table className="w-full border-collapse table-fixed">
-              <colgroup>
-                <col className="w-[16%]" />
-                <col className="w-[8%]" />
-                <col className="w-[16%]" />
-                <col className="w-[8%]" />
-                <col className="w-[16%]" />
-                <col className="w-[8%]" />
-                <col className="w-[16%]" />
-                <col className="w-[8%]" />
-                <col className="w-[16%]" />
-              </colgroup>
-              <thead>
-                <tr className="text-sm bg-da-primary-500 text-white uppercase">
-                  <th
-                    colSpan={3}
-                    className="border border-r-transparent font-semibold p-2 "
-                  >
-                    Off-board
-                  </th>
-                  <th className="border border-r-transparent"></th>
-                  <th colSpan={5} className="border font-semibold p-2">
-                    On-board
-                  </th>
-                </tr>
-                <tr className="text-xs text-da-gray-dark uppercase">
-                  <th className="border p-2">Smart Phone</th>
+            <>
+              <div className="flex items-center justify-between border-b pb-2 mb-4 ">
+                <h2 className="text-sm font-semibold text-da-primary-500">
+                  End-to-End Flow: Smart Trunk
+                </h2>
 
-                  <th className="border p-2">
-                    <DaTooltip content="Phone to Cloud" className="normal-case">
-                      <div className="cursor-pointer">p2c</div>
-                    </DaTooltip>
-                  </th>
+                <DaButton
+                  onClick={() => setIsEditing(!isEditing)}
+                  className="w-[60px]"
+                  variant="solid"
+                  size="sm"
+                >
+                  {isEditing ? 'Cancel' : 'Edit'}
+                </DaButton>
+              </div>
+              <table className="w-full border-collapse table-fixed">
+                <colgroup>
+                  <col className="w-[16%]" />
+                  <col className="w-[8%]" />
+                  <col className="w-[16%]" />
+                  <col className="w-[8%]" />
+                  <col className="w-[16%]" />
+                  <col className="w-[8%]" />
+                  <col className="w-[16%]" />
+                  <col className="w-[8%]" />
+                  <col className="w-[16%]" />
+                </colgroup>
+                <thead>
+                  <tr className="text-sm bg-da-primary-500 text-white uppercase">
+                    <th
+                      colSpan={3}
+                      className="border border-r-transparent font-semibold p-2 "
+                    >
+                      Off-board
+                    </th>
+                    <th className="border border-r-transparent"></th>
+                    <th colSpan={5} className="border font-semibold p-2">
+                      On-board
+                    </th>
+                  </tr>
+                  <tr className="text-xs text-da-gray-dark uppercase">
+                    <th className="border p-2">Smart Phone</th>
 
-                  <th className="border p-2">Cloud</th>
-                  <th className="border p-2 border-x-2 border-x-da-primary-500">
-                    <DaTooltip
-                      content="Vehicle to Cloud"
-                      className="normal-case"
-                    >
-                      <div className="cursor-pointer">v2c</div>
-                    </DaTooltip>
-                  </th>
-                  <th className="border p-2">SDV Runtime</th>
-                  <th className="border p-2">
-                    <DaTooltip
-                      content="System to System"
-                      className="normal-case"
-                    >
-                      <div className="cursor-pointer">s2s</div>
-                    </DaTooltip>
-                  </th>
-                  <th className="border p-2">Embedded</th>
-                  <th className="border p-2">
-                    <DaTooltip content="System to ECU" className="normal-case">
-                      <div className="cursor-pointer">s2e</div>
-                    </DaTooltip>
-                  </th>
-                  <th className="border p-2">Sensors/Actuators</th>
-                </tr>
-              </thead>
-              <tbody>
-                {flowData.map((step, stepIndex) => (
-                  <React.Fragment key={stepIndex}>
-                    <tr>
-                      <td
-                        colSpan={9}
-                        className="border p-2 font-semibold bg-da-primary-100 text-da-primary-500"
+                    <th className="border p-2">
+                      <DaTooltip
+                        content="Phone to Cloud"
+                        className="normal-case"
                       >
-                        {step.title}
-                      </td>
-                    </tr>
-                    {step.flows.map((flow, flowIndex) => (
-                      <tr key={flowIndex}>
-                        <td className="border p-2 text-center">
-                          {flow.offBoard.smartPhone}
-                        </td>
-                        <td className="border p-2 text-center">
-                          <SignalFlowCell flow={flow.offBoard.p2c} />
-                        </td>
-                        <td className="border p-2 text-center">
-                          {flow.offBoard.cloud}
-                        </td>
-                        <td className="border border-x-2 border-x-da-primary-500 p-2 text-center">
-                          <SignalFlowCell flow={flow.v2c} />
-                        </td>
-                        <td className="border p-2 text-center">
-                          {flow.onBoard.sdvRuntime}
-                        </td>
-                        <td className="border p-2 text-center">
-                          <SignalFlowCell flow={flow.onBoard.s2s} />
-                        </td>
-                        <td className="border p-2 text-center">
-                          {flow.onBoard.embedded}
-                        </td>
-                        <td className="border p-2 text-center">
-                          <SignalFlowCell flow={flow.onBoard.s2e} />
-                        </td>
-                        <td className="border p-2 text-center">
-                          {flow.onBoard.sensors}
+                        <div className="cursor-pointer">p2c</div>
+                      </DaTooltip>
+                    </th>
+
+                    <th className="border p-2">Cloud</th>
+                    <th className="border p-2 border-x-2 border-x-da-primary-500">
+                      <DaTooltip
+                        content="Vehicle to Cloud"
+                        className="normal-case"
+                      >
+                        <div className="cursor-pointer">v2c</div>
+                      </DaTooltip>
+                    </th>
+                    <th className="border p-2">SDV Runtime</th>
+                    <th className="border p-2">
+                      <DaTooltip
+                        content="System to System"
+                        className="normal-case"
+                      >
+                        <div className="cursor-pointer">s2s</div>
+                      </DaTooltip>
+                    </th>
+                    <th className="border p-2">Embedded</th>
+                    <th className="border p-2">
+                      <DaTooltip
+                        content="System to ECU"
+                        className="normal-case"
+                      >
+                        <div className="cursor-pointer">s2e</div>
+                      </DaTooltip>
+                    </th>
+                    <th className="border p-2">Sensors/Actuators</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {flowData.map((step, stepIndex) => (
+                    <React.Fragment key={stepIndex}>
+                      <tr>
+                        <td
+                          colSpan={9}
+                          className="border p-2 font-semibold bg-da-primary-100 text-da-primary-500"
+                        >
+                          {step.title}
                         </td>
                       </tr>
-                    ))}
-                  </React.Fragment>
-                ))}
-              </tbody>
-            </table>
+                      {step.flows.map((flow, flowIndex) => (
+                        <tr key={flowIndex}>
+                          <td className="border p-2 text-center">
+                            {flow.offBoard.smartPhone}
+                          </td>
+                          <td className="border p-2 text-center">
+                            <SignalFlowCell flow={flow.offBoard.p2c} />
+                          </td>
+                          <td className="border p-2 text-center">
+                            {flow.offBoard.cloud}
+                          </td>
+                          <td className="border border-x-2 border-x-da-primary-500 p-2 text-center">
+                            <SignalFlowCell flow={flow.v2c} />
+                          </td>
+                          <td className="border p-2 text-center">
+                            {flow.onBoard.sdvRuntime}
+                          </td>
+                          <td className="border p-2 text-center">
+                            <SignalFlowCell flow={flow.onBoard.s2s} />
+                          </td>
+                          <td className="border p-2 text-center">
+                            {flow.onBoard.embedded}
+                          </td>
+                          <td className="border p-2 text-center">
+                            <SignalFlowCell flow={flow.onBoard.s2e} />
+                          </td>
+                          <td className="border p-2 text-center">
+                            {flow.onBoard.sensors}
+                          </td>
+                        </tr>
+                      ))}
+                    </React.Fragment>
+                  ))}
+                </tbody>
+              </table>
+            </>
           )}
         </div>
       </div>
